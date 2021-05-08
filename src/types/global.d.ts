541a9eee6516588bd2d7b77bc6ebe4ca794ecd89
Welly Shen <hivoid19@gmail.com>
2021-05-09 03:57:24 +0800

    Refactor(types): remove unnecessary type

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,11 +1,1 @@
 declare const __DEV__: boolean;
-
-interface Window {
-  requestIdleCallback: (
-    callback: (deadline: {
-      readonly didTimeout: boolean;
-      timeRemaining: () => number;
-    }) => void,
-    options?: { timeout: number }
-  ) => any;
-}
