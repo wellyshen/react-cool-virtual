f93c48957344a61905690a70d1e24397d5ff2f56
Welly Shen <hivoid19@gmail.com>
2021-05-13 17:50:14 +0800

    Refactor: rename `useResizeObserver` to `useObserver`

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,4 +3,4 @@ export { default as findNearestBinarySearch } from "./findNearestBinarySearch";
 export { default as invariant } from "./invariant";
 export { default as useIsoLayoutEffect } from "./useIsoLayoutEffect";
 export { default as useLatest } from "./useLatest";
-export { default as useResizeObserver } from "./useResizeObserver";
+export { default as useObserver } from "./useObserver";
