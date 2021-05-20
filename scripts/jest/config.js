c75044bf9e424b7a323d66b80c03b2c6d65d4c71
Welly Shen <hivoid19@gmail.com>
2021-05-21 00:41:17 +0800

    Chore(jest): ignore entries from code coverage

--- scripts/jest/config.js
+++ scripts/jest/config.js
@@ -2,6 +2,11 @@ module.exports = {
   preset: "ts-jest",
   rootDir: "../../",
   setupFilesAfterEnv: ["<rootDir>/scripts/jest/setup.ts"],
-  collectCoverageFrom: ["src/**/*.ts", "!src/types/*"],
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!**/index.ts",
+    "!**/index.umd.ts",
+    "!src/types/*",
+  ],
   globals: { __DEV__: true },
 };
