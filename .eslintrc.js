600ded7d05e2e5ec211cb5b46abc31d37fd3eb20
Welly Shen <hivoid19@gmail.com>
2021-05-21 18:40:13 +0800

    Chore(eslint): remove warn from rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,6 @@
 module.exports = {
   extends: ["welly"],
   rules: {
-    "no-console": [
-      "warn",
-      {
-        allow: ["warn", "error"],
-      },
-    ],
     "no-bitwise": "off",
     "no-param-reassign": "off",
     "no-underscore-dangle": "off",
