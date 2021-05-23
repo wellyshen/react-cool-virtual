e839ed5e4a032bb3f127935dd6b45e03d0fadcee
Welly <hivoid19@gmail.com>
2021-05-23 21:56:51 +0800

    Chore: update example

--- examples/dynamic-size/index.js
+++ examples/dynamic-size/index.js
@@ -6,12 +6,12 @@ import Grid from "./Grid";
 import "./styles.scss";
 
 const getSizes = (min) =>
-  new Array(1000).fill().map(() => min + Math.round(Math.random() * 50));
+  new Array(1000).fill().map(() => min + Math.round(Math.random() * 100));
 
 const App = () => (
   <div className="app">
     <h4>Row</h4>
-    <Row rowHeights={getSizes(25)} />
+    <Row rowHeights={getSizes(35)} />
     <br />
     <br />
     <h4>Column</h4>
@@ -19,7 +19,7 @@ const App = () => (
     <br />
     <br />
     <h4>Grid</h4>
-    <Grid rowHeights={getSizes(25)} colWidths={getSizes(100)} />
+    <Grid rowHeights={getSizes(35)} colWidths={getSizes(100)} />
     <br />
   </div>
 );
