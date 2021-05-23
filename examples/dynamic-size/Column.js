10d92eb1064c4c19edf7519b011128ee11db1ba4
Welly Shen <hivoid19@gmail.com>
2021-05-23 19:59:17 +0800

    Chore(examples): add dynamic size

---
+++ examples/dynamic-size/Column.js
@@ -0,0 +1,35 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+
+import useVirtual from "react-cool-virtual";
+
+import "./styles.scss";
+
+const HorizontalList = ({ colWidths }) => {
+  const { outerRef, innerRef, items } = useVirtual({
+    horizontal: true,
+    itemCount: colWidths.length,
+    itemSize: (idx) => colWidths[idx]
+  });
+
+  return (
+    <div
+      className="outer"
+      style={{ width: "300px", height: "75px", overflow: "auto" }}
+      ref={outerRef}
+    >
+      <div ref={innerRef} style={{ display: "flex" }}>
+        {items.map(({ index, size }) => (
+          <div
+            key={index}
+            className={`item ${index % 2 && "light"}`}
+            style={{ minWidth: `${size}px`, height: "75px" }}
+          >
+            ♻️ {index}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HorizontalList;
