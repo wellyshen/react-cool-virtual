488d036220571b1ebba67b3139b5bc9e3994a797
Welly Shen <hivoid19@gmail.com>
2021-05-25 12:36:11 +0800

    chore(examples): add real-time resize example

---
+++ examples/real-time-resize/Grid.js
@@ -0,0 +1,66 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+
+import { Fragment } from "react";
+import useVirtual from "react-cool-virtual";
+
+import "./styles.scss";
+
+const Grid = ({ rowHeights, colWidths }) => {
+  const row = useVirtual({
+    itemCount: rowHeights.length,
+    itemSize: (idx) => rowHeights[idx]
+  });
+  const col = useVirtual({
+    horizontal: true,
+    itemCount: colWidths.length,
+    itemSize: (idx) => colWidths[idx]
+  });
+
+  return (
+    <div
+      className="outer"
+      style={{ width: "400px", height: "400px", overflow: "auto" }}
+      ref={(el) => {
+        row.outerRef.current = el;
+        col.outerRef.current = el;
+      }}
+    >
+      <div
+        style={{ position: "relative" }}
+        ref={(el) => {
+          row.innerRef.current = el;
+          col.innerRef.current = el;
+        }}
+      >
+        {row.items.map((rowItem) => (
+          <Fragment key={rowItem.index}>
+            {col.items.map((colItem) => (
+              <div
+                key={colItem.index}
+                className={`item ${
+                  rowItem.index % 2
+                    ? colItem.index % 2
+                      ? "dark"
+                      : ""
+                    : !(colItem.index % 2)
+                    ? "dark"
+                    : ""
+                }`}
+                style={{
+                  position: "absolute",
+                  height: `${rowItem.size}px`,
+                  width: `${colItem.size}px`,
+                  transform: `translateX(${colItem.start}px) translateY(${rowItem.start}px)`
+                }}
+              >
+                ♻️ {rowItem.index}, {colItem.index}
+              </div>
+            ))}
+          </Fragment>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Grid;
---
+++ examples/real-time-resize/Row.js
@@ -0,0 +1,34 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+
+import useVirtual from "react-cool-virtual";
+
+import "./styles.scss";
+
+const Row = ({ rowHeights }) => {
+  const { outerRef, innerRef, items } = useVirtual({
+    itemCount: rowHeights.length,
+    itemSize: (idx) => rowHeights[idx]
+  });
+
+  return (
+    <div
+      className="outer"
+      style={{ width: "300px", height: "300px", overflow: "auto" }}
+      ref={outerRef}
+    >
+      <div ref={innerRef}>
+        {items.map(({ index, size }) => (
+          <div
+            key={index}
+            className={`item ${index % 2 ? "dark" : ""}`}
+            style={{ height: `${size}px` }}
+          >
+            ♻️ {index}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
