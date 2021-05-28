abf58cbd6d91bbc88bb5cc3c4bb3288282e9dd96
Welly <hivoid19@gmail.com>
2021-05-28 19:51:09 +0800

    Chore: update examples

--- examples/real-time-resize/index.js
+++ examples/real-time-resize/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+
 import { render } from "react-dom";
 
 import Row from "./Row";
@@ -12,7 +14,7 @@ const App = () => (
   <div className="app">
     <p>
       💡 CodeSandbox might be caching the wrong version of React Cool Virtual.
-      You can use a specific version to see the examples.
+      You can manually specify to the latest version to see the examples.
     </p>
     <br />
     <h4>Row</h4>
