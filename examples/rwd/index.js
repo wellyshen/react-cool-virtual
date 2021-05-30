9d0fd6d9d84f9cf8877601a282569280a9f3c78b
Welly <hivoid19@gmail.com>
2021-05-30 16:09:18 +0800

    chore(examples): update RWD example

--- examples/rwd/index.js
+++ examples/rwd/index.js
@@ -1,31 +1,45 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
 
 import { render } from "react-dom";
+import useVirtual from "react-cool-virtual";
 
-import Row from "./Row";
-import Column from "./Column";
-import Grid from "./Grid";
 import "./styles.scss";
 
-const App = () => (
-  <div className="app">
-    <p>
-      💡 CodeSandbox might be caching the wrong version of React Cool Virtual.
-      You can manually specify to the latest version to see the examples.
-    </p>
-    <br />
-    <h4>Row</h4>
-    <Row />
-    <br />
-    <br />
-    <h4>Column</h4>
-    <Column />
-    <br />
-    <br />
-    <h4>Grid</h4>
-    <Grid />
-    <br />
-  </div>
-);
+const App = () => {
+  const { outerRef, innerRef, items } = useVirtual({
+    itemCount: 1000,
+    // Use the outer's width (2nd parameter) to adjust the item's size
+    itemSize: (_, width) => (width > 400 ? 50 : 100)
+  });
+
+  return (
+    <div className="app">
+      <p>
+        💡 CodeSandbox might be caching the wrong version of React Cool Virtual.
+        You can manually specify to the latest version to see the examples.
+      </p>
+      <br />
+      <div
+        className="outer"
+        style={{ width: "300px", height: "400px", overflow: "auto" }}
+        ref={outerRef}
+      >
+        <div ref={innerRef}>
+          {/* We can also access the outer's width here */}
+          {items.map(({ index, size, width }) => (
+            <div
+              key={index}
+              className={`item ${index % 2 ? "dark" : ""}`}
+              style={{ height: `${size}px` }}
+            >
+              📏 {width}
+            </div>
+          ))}
+        </div>
+      </div>
+      <br />
+    </div>
+  );
+};
 
 render(<App />, document.getElementById("root"));
