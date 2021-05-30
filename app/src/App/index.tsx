958b751e464694b5825624fcfc06ecdf40de9197
Welly Shen <hivoid19@gmail.com>
2021-05-31 01:33:17 +0800

    chore(app): try something...

--- app/src/App/index.tsx
+++ app/src/App/index.tsx
@@ -2,45 +2,17 @@
 
 import { useState } from "react";
 import useVirtual from "react-cool-virtual";
-import axios from "axios";
 
 import "normalize.css";
 import styles from "./styles.module.scss";
 
-const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
-
-const itemLoadedArr: any[] = [];
-
-// @ts-expect-error
-const loadData = async ({ loadIndex }, setComments) => {
-  itemLoadedArr[loadIndex] = true;
-
-  try {
-    await sleep(2500);
-
-    const { data: comments } = await axios(
-      `https://jsonplaceholder.typicode.com/comments?postId=${loadIndex + 1}`
-    );
-
-    // @ts-expect-error
-    setComments((prevComments) => [...prevComments, ...comments]);
-  } catch (err) {
-    itemLoadedArr[loadIndex] = false;
-    loadData({ loadIndex }, setComments);
-  }
-};
-
 export default (): JSX.Element => {
-  const [comments, setComments] = useState<any[]>([]);
+  const [sz, setSz] = useState(50);
   const { outerRef, innerRef, items } = useVirtual<
     HTMLDivElement,
     HTMLDivElement
   >({
-    itemCount: 500,
-    // itemSize: 50,
-    loadMoreThreshold: 5,
-    isItemLoaded: (loadIndex) => itemLoadedArr[loadIndex],
-    loadMore: (options) => loadData(options, setComments),
+    itemCount: 50,
   });
 
   return (
@@ -55,14 +27,20 @@ export default (): JSX.Element => {
             <div
               key={index}
               className={`${styles.item} ${index % 2 ? styles.dark : ""}`}
-              style={{ padding: "16px", minHeight: "50px" }}
+              style={{ height: `${index === 1 ? sz : size}px` }}
               ref={measureRef}
             >
-              {comments[index]?.body || "⏳ Loading..."}
+              {index}
             </div>
           ))}
         </div>
       </div>
+      <button
+        type="button"
+        onClick={() => setSz((prevSz) => (prevSz === 50 ? 200 : 50))}
+      >
+        Resize
+      </button>
     </div>
   );
 };
