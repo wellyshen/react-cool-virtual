04d76a59dacfe261198f60559bb3a5bb2df9564c
Welly <hivoid19@gmail.com>
2021-05-31 19:15:19 +0800

    chore: update exmaples

--- examples/scroll-to-controls/index.js
+++ examples/scroll-to-controls/index.js
@@ -9,8 +9,9 @@ import "./styles.scss";
 const App = () => (
   <div className="app">
     <p>
-      💡 CodeSandbox might be caching the wrong version of React Cool Virtual.
-      You can manually specify to the latest version to see the examples.
+      💡 CodeSandbox might be <b>caching the wrong version</b> of React Cool
+      Virtual. You can <b>manually specify to the latest version</b> to see the
+      examples.
     </p>
     <br />
     <h4>Fixed Size</h4>
