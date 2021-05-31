14cc031e01851876b867fe1f0c42717213f0bac4
Welly Shen <hivoid19@gmail.com>
2021-05-31 13:09:07 +0800

    chore(app): try something...

--- app/src/App/index.tsx
+++ app/src/App/index.tsx
@@ -6,13 +6,22 @@ import useVirtual from "react-cool-virtual";
 import "normalize.css";
 import styles from "./styles.module.scss";
 
+const getData = (num: number) =>
+  new Array(num).fill({}).map((_, idx) => ({
+    txt: idx,
+    size: Math.floor(25 + Math.random() * 100),
+  }));
+
+const mockData = getData(50);
+
 export default (): JSX.Element => {
   const [sz, setSz] = useState(50);
   const { outerRef, innerRef, items } = useVirtual<
     HTMLDivElement,
     HTMLDivElement
   >({
-    itemCount: 50,
+    itemCount: mockData.length,
+    useIsScrolling: true,
     itemSize: (_, width) => (width > 600 ? 100 : 50),
   });
 
@@ -20,14 +29,16 @@ export default (): JSX.Element => {
     <div className={styles.app}>
       <div className={styles.outer} ref={outerRef}>
         <div ref={innerRef}>
-          {items.map(({ index, size, measureRef }) => (
+          {items.map(({ index, size, isScrolling, measureRef }) => (
             <div
               key={index}
               className={`${styles.item} ${index % 2 ? styles.dark : ""}`}
-              style={{ height: `${size}px` }}
-              // ref={measureRef}
+              // style={{ height: `${mockData[index].size}px` }}
+              style={{ height: `${index === 10 ? sz : 50}px` }}
+              ref={measureRef}
             >
               {index}
+              {/* {isScrolling ? "Scrolling... " : index} */}
             </div>
           ))}
         </div>
