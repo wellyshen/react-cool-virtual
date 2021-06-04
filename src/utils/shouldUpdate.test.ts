03bedeffd9f4a1dc7eca960ea704d7c223e07d11
Welly Shen <hivoid19@gmail.com>
2021-06-04 18:38:53 +0800

    test: add testing for `shouldUpdate`

---
+++ src/utils/shouldUpdate.test.ts
@@ -0,0 +1,20 @@
+import shouldUpdate from "./shouldUpdate";
+
+describe("shouldUpdate", () => {
+  it("should work correctly", () => {
+    const a = {
+      index: 0,
+      start: 0,
+      size: 0,
+      width: 0,
+      isScrolling: false,
+      measureRef: () => null,
+    };
+    const b = { ...a, measureRef: () => null };
+    expect(shouldUpdate([a], [b], {})).toBeTruthy();
+    expect(shouldUpdate([a], [b], { measureRef: true })).toBeFalsy();
+    expect(
+      shouldUpdate([a], [{ ...b, index: 1 }], { measureRef: true })
+    ).toBeTruthy();
+  });
+});
