6303e8a707723f7575b22c331ce03000060f0d7b
Welly Shen <hivoid19@gmail.com>
2021-06-04 17:40:07 +0800

    chore(app): try something...

--- app/src/App/index.tsx
+++ app/src/App/index.tsx
@@ -1,138 +1,92 @@
-/* eslint-disable compat/compat */
+/* eslint-disable jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events */
 
-import useVirtual from "react-cool-virtual";
-
-import styles from "./styles.module.scss";
-
-const getItems = (num: number) =>
-  new Array(num).fill({}).map((_, idx) => ({
-    txt: idx,
-    size: Math.floor(25 + Math.random() * 100),
-  }));
-
-const mockData = getItems(100);
-
-export default () => {
-  const { outerRef, innerRef, items, scrollToItem } = useVirtual<
-    HTMLDivElement,
-    HTMLDivElement
-  >({ itemCount: mockData.length });
-
-  return (
-    <div className={styles.app}>
-      <div className={styles.outer} ref={outerRef}>
-        <div ref={innerRef}>
-          {items.map(({ index, size, measureRef }) => (
-            <div
-              key={index}
-              className={`${styles.item} ${index % 2 ? styles.dark : ""}`}
-              // style={{ height: `${50}px` }}
-              style={{ height: `${mockData[index].size}px` }}
-              ref={measureRef}
-            >
-              {index}
-            </div>
-          ))}
-        </div>
-      </div>
-      <button
-        type="button"
-        onClick={() =>
-          scrollToItem({ index: 50, smooth: false, align: "auto" }, () =>
-            console.log("DONE!")
-          )
-        }
-      >
-        Scroll To Item
-      </button>
-    </div>
-  );
-};
-
-/* import { Fragment, useState } from "react";
+import { useState, forwardRef } from "react";
 import useVirtual from "react-cool-virtual";
 import axios from "axios";
 
 import styles from "./styles.module.scss";
 
-const TOTAL_COMMENTS = 500;
-const BATCH_COMMENTS = 5;
-const isItemLoadedArr: any[] = [];
+const AccordionItem = forwardRef(({ height, ...rest }: any, ref: any) => {
+  const [h, setH] = useState<number>(height);
 
+  return (
+    <div
+      {...rest}
+      style={{ height: `${h}px`, cursor: "pointer", padding: "0" }}
+      ref={ref}
+      onClick={() => setH((prevH) => (prevH === 120 ? 240 : 120))}
+    >
+      🪗 Real-time resize
+    </div>
+  );
+});
+
+// eslint-disable-next-line compat/compat
 const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
-const loadData = async ({ loadIndex }: any, setComments: any) => {
-  // Set the state of a batch items as `true`
-  // to avoid the callback from being invoked repeatedly
-  isItemLoadedArr[loadIndex] = true;
-
-  try {
-    // Simulating a slow network
-    await sleep(2500);
-
-    const { data: comments } = await axios(
-      `https://jsonplaceholder.typicode.com/comments?postId=${loadIndex + 1}`
-    );
-
-    setComments((prevComments: any) => [...prevComments, ...comments]);
-  } catch (err) {
-    // If there's an error set the state back to `false`
-    isItemLoadedArr[loadIndex] = false;
-    // Then try again
-    loadData({ loadIndex }, setComments);
-  }
-};
-
-const Loading = () => <div className="item">⏳ Loading...</div>;
+const isItemLoadedArr: any[] = [];
 
 export default () => {
+  const [outerSize, setOutersize] = useState<{
+    width?: number;
+    height?: number;
+  }>({});
   const [comments, setComments] = useState<any[]>([]);
   const { outerRef, innerRef, items } = useVirtual<
     HTMLDivElement,
     HTMLDivElement
   >({
-    // // Provide the number of comments
-    // itemCount: comments.length,
-    itemCount: 500,
-    // Estimated item size (with padding)
-    // itemSize: 122,
-    // Starts to pre-fetch data when the user scrolls within every 5 items
-    // e.g. 1-5, 6-10 and so on (default = 15)
-    loadMoreCount: BATCH_COMMENTS,
-    // Provide the loaded state for a batch items to tell the hook
-    // whether the `loadMore` should be triggered or not
-    isItemLoaded: (loadIndex) => isItemLoadedArr[loadIndex],
-    // The callback will be invoked when more data needs to be loaded
-    loadMore: (e) => loadData(e, setComments),
+    itemCount: 60,
+    itemSize: 120,
+    loadMoreCount: 5,
+    isItemLoaded: (idx) => isItemLoadedArr[idx],
+    loadMore: async ({ loadIndex }) => {
+      if (loadIndex >= 10) return;
+
+      isItemLoadedArr[loadIndex] = true;
+
+      await sleep(2000);
+      const { data } = await axios(
+        `https://jsonplaceholder.typicode.com/comments?postId=${loadIndex + 1}`
+      );
+
+      setComments((prevComments) => [...prevComments, ...data]);
+    },
+    onResize: setOutersize,
   });
 
   return (
     <div className={styles.app}>
+      {Object.keys(outerSize).length ? (
+        <p>
+          Container size: {Math.floor(outerSize.width || 0)} x{" "}
+          {Math.floor(outerSize.height || 0)}
+        </p>
+      ) : null}
       <div className={styles.outer} ref={outerRef}>
         <div ref={innerRef}>
-          {items.length ? (
-            items.map(({ index, measureRef }) => {
-              const len = comments.length;
-              const showLoading = index === len - 1 && len < TOTAL_COMMENTS;
-
-              return (
-                <Fragment key={index}>
-                  <div
-                    className={`${styles.item} ${index % 2 ? styles.dark : ""}`}
-                    style={{ padding: "16px" }}
-                    ref={measureRef} // Used to measure the unknown item size
-                  >
-                    {comments[index]?.body || "Loading..."}
-                  </div>
-                  {showLoading && <Loading />}
-                </Fragment>
-              );
-            })
-          ) : (
-            <Loading />
+          {items.map(({ index, size, measureRef }) =>
+            index <= 49 ? (
+              <div
+                key={index}
+                className={`${styles.item} ${index % 2 ? styles.dark : ""}`}
+                ref={measureRef}
+              >
+                {comments[index]
+                  ? `${comments[index].id}. ${comments[index].body}`
+                  : "⏳ Loading..."}
+              </div>
+            ) : (
+              <AccordionItem
+                key={index}
+                className={`${styles.item} ${index % 2 ? styles.dark : ""}`}
+                height={size}
+                ref={measureRef}
+              />
+            )
           )}
         </div>
       </div>
     </div>
   );
-}; */
+};
