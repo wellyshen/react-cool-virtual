979ca196a1e1189b223e220fc077ea92ad765f6c
Welly <hivoid19@gmail.com>
2021-06-13 16:43:06 +0800

    chore(examples): update RCF example

--- examples/react-cool-form/index.js
+++ examples/react-cool-form/index.js
@@ -1,28 +1,58 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
 
 import { render } from "react-dom";
+import useVirtual from "react-cool-virtual";
+import { useForm } from "react-cool-form";
 
-import Form from "./Form";
-import RCF from "./RCF";
 import "./styles.scss";
 
-const App = () => (
-  <div className="app">
-    <p>
-      💡 CodeSandbox might be <b>caching the old version</b> of React Cool
-      Virtual. You can <b>manually specify to the latest version</b> to see the
-      examples.
-    </p>
-    <br />
-    <h4>Form</h4>
-    <Form />
-    <br />
-    <br />
-    <h4>React Cool Form</h4>
-    <RCF />
-    <br />
-    <br />
-  </div>
-);
+const defaultValues = new Array(20).fill(false);
+
+const App = () => {
+  const { outerRef, innerRef, items } = useVirtual({
+    itemCount: defaultValues.length
+  });
+  const { form } = useForm({
+    defaultValues: { todo: defaultValues },
+    removeOnUnmounted: false, // To keep the value of unmounted fields
+    onSubmit: (formData) => alert(JSON.stringify(formData, undefined, 2))
+  });
+
+  return (
+    <div className="app">
+      <p>
+        💡 CodeSandbox might be <b>caching the old version</b> of React Cool
+        Virtual. You can <b>manually specify to the latest version</b> to see
+        the examples.
+      </p>
+      <br />
+      <form ref={form}>
+        <div
+          className="outer"
+          style={{ width: "300px", height: "300px", overflow: "auto" }}
+          ref={outerRef}
+        >
+          <div ref={innerRef}>
+            {items.map(({ index, size }) => (
+              <div
+                key={index}
+                className={`item ${index % 2 ? "dark" : ""}`}
+                style={{ height: `${size}px` }}
+              >
+                <input
+                  id={`todo-${index}`}
+                  name={`todo[${index}]`}
+                  type="checkbox"
+                />
+                <label htmlFor={`todo-${index}`}>{index}. I'd like to...</label>
+              </div>
+            ))}
+          </div>
+        </div>
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
 
 render(<App />, document.getElementById("root"));
