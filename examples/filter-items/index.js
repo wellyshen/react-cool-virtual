7b61fed2fb8ff0cdacfbbd17b93a58d63d29057f
Welly Shen <hivoid19@gmail.com>
2021-06-19 20:41:05 +0800

    chore(examples): add filter items example

---
+++ examples/filter-items/index.js
@@ -0,0 +1,33 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+
+import { render } from "react-dom";
+
+import Row from "./Row";
+import Column from "./Column";
+import Grid from "./Grid";
+import "./styles.scss";
+
+const App = () => (
+  <div className="app">
+    <p>
+      💡 CodeSandbox might be <b>caching the old version</b> of React Cool
+      Virtual. You can <b>manually specify to the latest version</b> to see the
+      examples.
+    </p>
+    <br />
+    <h4>Row</h4>
+    <Row />
+    <br />
+    <br />
+    <h4>Column</h4>
+    <Column />
+    <br />
+    <br />
+    <h4>Grid</h4>
+    <Grid />
+    <br />
+    <br />
+  </div>
+);
+
+render(<App />, document.getElementById("root"));
