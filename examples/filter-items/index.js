0995f3ed518f319e10e7ca26c0fba39aa717f483
Welly <hivoid19@gmail.com>
2021-06-19 20:57:35 +0800

    chore(examples): update filter-items example

--- examples/filter-items/index.js
+++ examples/filter-items/index.js
@@ -1,33 +1,51 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
 
+import { useState } from "react";
 import { render } from "react-dom";
+import useVirtual from "react-cool-virtual";
 
-import Row from "./Row";
-import Column from "./Column";
-import Grid from "./Grid";
 import "./styles.scss";
 
-const App = () => (
-  <div className="app">
-    <p>
-      💡 CodeSandbox might be <b>caching the old version</b> of React Cool
-      Virtual. You can <b>manually specify to the latest version</b> to see the
-      examples.
-    </p>
-    <br />
-    <h4>Row</h4>
-    <Row />
-    <br />
-    <br />
-    <h4>Column</h4>
-    <Column />
-    <br />
-    <br />
-    <h4>Grid</h4>
-    <Grid />
-    <br />
-    <br />
-  </div>
-);
+const App = () => {
+  const [itemCount, setItemCount] = useState(100);
+  const { outerRef, innerRef, items } = useVirtual({
+    itemCount,
+    // Resets the scroll position when the `itemCount` changed (default = false)
+    resetScroll: true
+  });
+
+  return (
+    <div className="app">
+      <p>
+        💡 CodeSandbox might be <b>caching the old version</b> of React Cool
+        Virtual. You can <b>manually specify to the latest version</b> to see
+        the examples.
+      </p>
+      <br />
+      <select onChange={({ target }) => setItemCount(target.value)}>
+        <option value="100">100</option>
+        <option value="50">50</option>
+        <option value="25">25</option>
+      </select>
+      <div
+        className="outer"
+        style={{ width: "300px", height: "300px", overflow: "auto" }}
+        ref={outerRef}
+      >
+        <div ref={innerRef}>
+          {items.map(({ index, size }) => (
+            <div
+              key={index}
+              className={`item ${index % 2 ? "dark" : ""}`}
+              style={{ height: `${size}px` }}
+            >
+              ♻️ {index}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 render(<App />, document.getElementById("root"));
