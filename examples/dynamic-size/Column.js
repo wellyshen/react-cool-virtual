bc285f9a8abd7d43d721b29c5a65a2de609fea71
Welly <hivoid19@gmail.com>
2021-06-30 15:47:04 +0800

    chore(examples): use flex-basis

--- examples/dynamic-size/Column.js
+++ examples/dynamic-size/Column.js
@@ -22,7 +22,7 @@ const Column = ({ colWidths }) => {
           <div
             key={index}
             className={`item ${index % 2 ? "dark" : ""}`}
-            style={{ minWidth: `${colWidths[index]}px`, height: "75px" }}
+            style={{ flexBasis: `${colWidths[index]}px`, height: "75px" }}
             ref={measureRef} // It will measure the item size for us
           >
             📏 {size}
